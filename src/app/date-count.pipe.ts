4216276eb8943cf14dad6d7b414c8453f06693ff
SophiaNM <njerimurage92@outlook.com>
2018-05-28 14:41:38 +0300

    fixed date count to calculate hours passed

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -7,13 +7,14 @@ export class DateCountPipe implements PipeTransform {
 
   transform(value: any): number {
     let today:Date = new Date(); //get current date and time
-    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-    var dateDifference =Math.abs(value-todayWithNoTime )// returns value in milliseconds
-    const secondsInADay= 86400; //60 seconds*60 minutes in an hour *24 hours
+    // let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference =Math.abs(value-today)// returns value in milliseconds
+    const secondsInAnHour= 3600; //60 seconds*60 minutes in an hour
 
     var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
 
-    var dateCounter = dateDifferenceSeconds/secondsInADay;
+    var dateCounter = dateDifferenceSeconds/secondsInAnHour;
+    var dateCounter = dateCounter.toFixed(0);
 
     if (dateCounter >= 1){
         return dateCounter;
